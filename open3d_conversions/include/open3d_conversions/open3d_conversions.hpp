c426ec5b8158250df765263abba45ed2a95c2b3c
Alex Navarro <56406642+alexnavtt@users.noreply.github.com>
2024-07-09 16:12:49 -0600

    Add open3d::geometry::Image <-> sensor_msgs::msg::Image conversions (#31)
    * Added conversion between open3d image and sensor_msgs image
    
    * Added camera info conversions and move conversions
    
    * Updated README for image conversions
    
    * Formatting changes after running ament_cpplint and ament_uncrustify
    
    * Switched to using rcpputils for checking endianness
    
    * Fixed spacing errors
    
    * Added endianness correction moving from ros to open3d image
    
    * Added test cases for image conversions
    
    * Removed frame_id inputs from all functions
    
    * Filled in copyright statement comment lines to fix failing copyright test
    
    * Adjusted comments to account for the fact that frame_id is no longer used

--- open3d_conversions/include/open3d_conversions/open3d_conversions.hpp
+++ open3d_conversions/include/open3d_conversions/open3d_conversions.hpp
@@ -1,11 +1,11 @@
 // Copyright 2020 Autonomous Robots Lab, University of Nevada, Reno
-
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,18 +14,20 @@
 #ifndef OPEN3D_CONVERSIONS__OPEN3D_CONVERSIONS_HPP_
 #define OPEN3D_CONVERSIONS__OPEN3D_CONVERSIONS_HPP_
 
-// ROS2
-#include <sensor_msgs/msg/point_cloud2.hpp>
-#include <sensor_msgs/point_cloud2_iterator.hpp>
+// Eigen
+#include <Eigen/Dense>
 
 // Open3D
 #include <open3d/Open3D.h>
 
-// Eigen
-#include <Eigen/Dense>
-
 // C++
 #include <string>
+
+// ROS2
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
 
 #include "open3d_conversions/visibility_control.h"
 
@@ -33,16 +35,45 @@ namespace open3d_conversions
 {
 /**
  * @brief Copy data from a open3d::geometry::PointCloud to a
- * sensor_msgs::msg::PointCloud2
+ * sensor_msgs::msg::PointCloud2. The header field of the output
+ * pointcloud is not modified, and must be filled out separately
  *
  * @param pointcloud Reference to the open3d geometry PointCloud
  * @param ros_pc2 Reference to the sensor_msgs PointCloud2
- * @param frame_id The string to be placed in the frame_id of the PointCloud2
  */
 void open3dToRos(
   const open3d::geometry::PointCloud & pointcloud,
-  sensor_msgs::msg::PointCloud2 & ros_pc2,
-  std::string frame_id = "open3d_pointcloud");
+  sensor_msgs::msg::PointCloud2 & ros_pc2);
+
+/**
+ * @brief Copy data from a open3d::geometry::Image to a
+ * sensor_msgs::msg::Image. The header field of the output
+ * image is not modified, and must be filled out separately
+ *
+ * @param image Reference to the open3d geometry Image
+ * @param ros_img Reference to the sensor_msgs Image
+ * @param encoding The encoding of the image to use in ROS format
+ */
+void open3dToRos(
+  const open3d::geometry::Image & o3d_img,
+  sensor_msgs::msg::Image & ros_img,
+  std::string encoding);
+
+/**
+ * @brief Copy data from a open3d::camera::PinholeCameraIntrinsic
+ * to a sensor_msgs::msg::CameraInfo. The header field of the output
+ * camera info is not modified, and must be filled out separately
+ *
+ * @param intrinsic Reference to the camera intrinsic in open3d format
+ * @param ros_img Reference to the sensor_msgs CameraInfo
+ *
+ * @note Open3D does not record distortion parameters for its images, so
+ * the sensor_msgs Image be labelled as "plumb_bob" with D values of 0
+ */
+void open3dToRos(
+  const open3d::camera::PinholeCameraIntrinsic & intrinsic,
+  sensor_msgs::msg::CameraInfo & camera_info);
+
 /**
  * @brief Copy data from a sensor_msgs::msg::PointCloud2 to a
  * open3d::geometry::PointCloud
@@ -55,6 +86,59 @@ void rosToOpen3d(
   const sensor_msgs::msg::PointCloud2::SharedPtr & ros_pc2,
   open3d::geometry::PointCloud & o3d_pc,
   bool skip_colors = false);
+
+/**
+ * @brief Copy data from a sensor_msgs::msg::Image to a
+ * open3d::geometry::Image
+ *
+ * @param ros_img Reference to the sensor_msgs Image
+ * @param o3d_img Reference to the open3d geometry Image
+ */
+void rosToOpen3d(
+  const sensor_msgs::msg::Image & ros_img,
+  open3d::geometry::Image & o3d_img);
+
+/**
+ * @brief Copy data from a sensor_msgs::msg::CameraInfo
+ * to a open3d::camera::PinholeCameraIntrinsic
+ *
+ * @param ros_img Reference to the sensor_msgs CameraInfo to populate
+ * @param intrinsic Reference to the open3d PinholeCameraIntrinsic
+ */
+void rosToOpen3d(
+  const sensor_msgs::msg::CameraInfo & camera_info,
+  open3d::camera::PinholeCameraIntrinsic & intrinsic);
+
+/**
+ * @brief Move data from a open3d::geometry::Image to a
+ * sensor_msgs::msg::Image. The header field of the output
+ * image is not modified, and must be filled out separately
+ *
+ * @param o3d_img Reference to the open3d geometry Image
+ * @param ros_img Reference to the sensor_msgs Image
+ * @param encoding The encoding of the image to use in ROS format
+ *
+ * @note The Open3D image will be in a well defined state after this
+ * operation with width = height = 0, and an empty data vector
+ */
+void moveOpen3dToRos(
+  open3d::geometry::Image && o3d_img,
+  sensor_msgs::msg::Image & ros_img,
+  std::string encoding);
+
+/**
+ * @brief Move data from a sensor_msgs::msg::Image to a
+ * open3d::geometry::Image
+ *
+ * @param ros_img Reference to the sensor_msgs Image
+ * @param o3d_img Reference to the open3d geometry Image
+ *
+ * @note The sensor_msgs Image will be a well defined state after this
+ * operation with width = height = 0, and an empty data vector
+ */
+void moveRosToOpen3d(
+  sensor_msgs::msg::Image && ros_img,
+  open3d::geometry::Image & o3d_img);
 
 }  // namespace open3d_conversions
 
--- open3d_conversions/src/open3d_conversions.cpp
+++ open3d_conversions/src/open3d_conversions.cpp
@@ -1,11 +1,11 @@
 // Copyright 2020 Autonomous Robots Lab, University of Nevada, Reno
-
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,43 @@
 // C++
 #include <memory>
 #include <string>
+#include <sstream>
 
+#include <rcpputils/endian.hpp>
+#include <sensor_msgs/image_encodings.hpp>
 #include "open3d_conversions/open3d_conversions.hpp"
+
+// Verify that an encoding makes sense with a given image
+static void checkEncodingValidity(
+  const std::string & encoding,
+  const open3d::geometry::Image & o3d_img)
+{
+  const int expected_num_channels = sensor_msgs::image_encodings::numChannels(encoding);
+  const int expected_bytes_per_channel = sensor_msgs::image_encodings::bitDepth(encoding) / 8;
+
+  if (expected_num_channels != o3d_img.num_of_channels_) {
+    std::stringstream ss;
+    ss << "Mismatch between Open3D image encoding and desired embedded encoding"
+       << "You asked for \"" << encoding << "\" which has " << expected_num_channels
+       << " channels but the provided image had " << o3d_img.num_of_channels_ << " channels";
+    throw std::runtime_error(ss.str());
+  }
+
+  if (expected_bytes_per_channel != o3d_img.bytes_per_channel_) {
+    std::stringstream ss;
+    ss << "Mismatch between Open3D image encoding and desired embedded encoding"
+       << "You asked for \"" << encoding << "\" which has " << expected_bytes_per_channel
+       << " bytes per channel but the provided image had " << o3d_img.bytes_per_channel_
+       << " bytes per channel";
+    throw std::runtime_error(ss.str());
+  }
+}
 
 namespace open3d_conversions
 {
 void open3dToRos(
   const open3d::geometry::PointCloud & pointcloud,
-  sensor_msgs::msg::PointCloud2 & ros_pc2, std::string frame_id)
+  sensor_msgs::msg::PointCloud2 & ros_pc2)
 {
   sensor_msgs::PointCloud2Modifier modifier(ros_pc2);
   if (pointcloud.HasColors()) {
@@ -31,7 +60,6 @@ void open3dToRos(
     modifier.setPointCloud2FieldsByString(1, "xyz");
   }
   modifier.resize(pointcloud.points_.size());
-  ros_pc2.header.frame_id = frame_id;
   sensor_msgs::PointCloud2Iterator<float> ros_pc2_x(ros_pc2, "x");
   sensor_msgs::PointCloud2Iterator<float> ros_pc2_y(ros_pc2, "y");
   sensor_msgs::PointCloud2Iterator<float> ros_pc2_z(ros_pc2, "z");
@@ -62,6 +90,43 @@ void open3dToRos(
       *ros_pc2_z = point(2);
     }
   }
+}
+
+void open3dToRos(
+  const open3d::geometry::Image & o3d_img,
+  sensor_msgs::msg::Image & ros_img,
+  std::string encoding)
+{
+  checkEncodingValidity(encoding, o3d_img);
+  ros_img.encoding = encoding;
+  ros_img.height = o3d_img.height_;
+  ros_img.width = o3d_img.width_;
+  ros_img.step = o3d_img.BytesPerLine();
+  ros_img.data = o3d_img.data_;
+  ros_img.is_bigendian = rcpputils::endian::native == rcpputils::endian::big;
+}
+
+void open3dToRos(
+  const open3d::camera::PinholeCameraIntrinsic & intrinsic,
+  sensor_msgs::msg::CameraInfo & camera_info)
+{
+  // Intrinsic matrix is 3x3 row-major
+  std::fill(camera_info.k.begin(), camera_info.k.end(), 0.0);
+  std::fill(camera_info.p.begin(), camera_info.p.end(), 0.0);
+  for (std::size_t i = 0; i < 9; i++) {
+    camera_info.k[i] = intrinsic.intrinsic_matrix_(i / 3, i % 3);
+  }
+  // Assuming image from monocular camera
+  for (std::size_t i = 0; i < 12; i++) {
+    if (i % 4 < 3) {
+      camera_info.p[i] = intrinsic.intrinsic_matrix_(i / 4, i % 4);
+    }
+  }
+
+  // Open3d intrinsics do not contain distortion information
+  camera_info.distortion_model = "plumb_bob";
+  camera_info.d.resize(5);
+  std::fill(camera_info.d.begin(), camera_info.d.end(), 0.0);
 }
 
 void rosToOpen3d(
@@ -111,4 +176,104 @@ void rosToOpen3d(
     }
   }
 }
+
+void rosToOpen3d(
+  const sensor_msgs::msg::Image & ros_img,
+  open3d::geometry::Image & o3d_img)
+{
+  const int num_channels = sensor_msgs::image_encodings::numChannels(ros_img.encoding);
+  const int bytes_per_channel = sensor_msgs::image_encodings::bitDepth(ros_img.encoding) / 8;
+
+  o3d_img.Prepare(
+    ros_img.width,
+    ros_img.height,
+    num_channels,
+    bytes_per_channel
+  );
+
+  // Open3D requires the image to be in native endianness
+  if (ros_img.is_bigendian && rcpputils::endian::native == rcpputils::endian::big ||
+    !ros_img.is_bigendian && rcpputils::endian::native == rcpputils::endian::little ||
+    bytes_per_channel == 1)
+  {
+    o3d_img.data_ = ros_img.data;
+    return;
+  }
+
+  // Otherwise, flip the bytes of each channel entry
+  o3d_img.data_.clear();
+  o3d_img.data_.reserve(ros_img.data.size());
+  const std::ptrdiff_t data_step = bytes_per_channel * num_channels;
+  for (auto data_ptr = ros_img.data.begin(); data_ptr != ros_img.data.end();
+    std::advance(data_ptr, data_step))
+  {
+    for (std::ptrdiff_t channel_idx = 0; channel_idx < num_channels; ++channel_idx) {
+      const std::ptrdiff_t channel_start = channel_idx * bytes_per_channel;
+      for (std::ptrdiff_t byte_idx = 0; byte_idx < bytes_per_channel; ++byte_idx) {
+        const std::ptrdiff_t reverse_byte_idx = bytes_per_channel - byte_idx - 1;
+        o3d_img.data_.push_back(*(data_ptr + channel_start + reverse_byte_idx));
+      }
+    }
+  }
+}
+
+void rosToOpen3d(
+  const sensor_msgs::msg::CameraInfo & camera_info,
+  open3d::camera::PinholeCameraIntrinsic & intrinsic)
+{
+  intrinsic.width_ = camera_info.width;
+  intrinsic.height_ = camera_info.height;
+  for (std::size_t row = 0; row < 3; row++) {
+    for (std::size_t col = 0; col < 3; col++) {
+      intrinsic.intrinsic_matrix_(row, col) = camera_info.k[3 * row + col];
+    }
+  }
+}
+
+void moveOpen3dToRos(
+  open3d::geometry::Image && o3d_img,
+  sensor_msgs::msg::Image & ros_img,
+  std::string encoding)
+{
+  checkEncodingValidity(encoding, o3d_img);
+  ros_img.encoding = encoding;
+  ros_img.height = o3d_img.height_;
+  ros_img.width = o3d_img.width_;
+  ros_img.step = o3d_img.BytesPerLine();
+  ros_img.is_bigendian = rcpputils::endian::native == rcpputils::endian::big;
+  ros_img.data = std::move(o3d_img.data_);
+
+  // Make sure to leave everything in a well defined state
+  o3d_img.Clear();
+}
+
+void moveRosToOpen3d(
+  sensor_msgs::msg::Image && ros_img,
+  open3d::geometry::Image & o3d_img)
+{
+  // If the endianness does not match, we will need to perform a copy anyway
+  if ((ros_img.is_bigendian && rcpputils::endian::native == rcpputils::endian::little ||
+    !ros_img.is_bigendian && rcpputils::endian::native == rcpputils::endian::big) &&
+    sensor_msgs::image_encodings::bitDepth(ros_img.encoding) > 8)
+  {
+    rosToOpen3d(ros_img, o3d_img);
+    return;
+  }
+
+  o3d_img.Prepare(
+    ros_img.width,
+    ros_img.height,
+    sensor_msgs::image_encodings::numChannels(ros_img.encoding),
+    sensor_msgs::image_encodings::bitDepth(ros_img.encoding) / 8
+  );
+  o3d_img.data_ = std::move(ros_img.data);
+
+  // Make sure to leave everything in a well defined state
+  ros_img.data.clear();
+  ros_img.step = 0;
+  ros_img.height = 0;
+  ros_img.width = 0;
+  ros_img.encoding.clear();
+}
+
 }  // namespace open3d_conversions
